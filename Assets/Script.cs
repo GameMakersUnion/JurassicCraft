5b58c7ede0fd3156cacc317dec2d964f916ef9d2
Ian Nastajus <ian.nastajus@gmail.com>
2014-08-08 18:18:41 -0400

    removed junk. added sample spawn script

---
+++ Assets/Script.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(transform.position);
+        Debug.Log(transform.name);
+
+        GameObject go = Resources.Load<GameObject>("Trex");
+        Instantiate(go, this.transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
