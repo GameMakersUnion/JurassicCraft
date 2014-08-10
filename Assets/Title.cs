5a1d920e2222439f5b1f549390d2f16d44780e72
David Tenty <dtenty@ryerson.ca>
2014-08-10 15:16:06 -0400

    Added title screen

---
+++ Assets/Title.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)||Input.GetMouseButtonDown(1)) {
+			Application.LoadLevel("MainScene");		
+		}
+	}
+}
