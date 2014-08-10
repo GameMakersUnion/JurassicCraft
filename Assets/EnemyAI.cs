9cce05ea7396879d52a6bad05186818dc7dadaa2
David Tenty <dtenty@ryerson.ca>
2014-08-10 14:05:52 -0400

    Update ai ranges

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour {
 	public GameObject enemies;
 	public GameObject friends;
+
+	private int targetUnit;
+
 	// Use this for initialization
 	void Start () {
-
+		targetUnit = 0;
 	}
 	
 	// Update is called once per frame
@@ -15,7 +18,8 @@ public class EnemyAI : MonoBehaviour {
 		{
 			Vector3 target = Vector3.zero;
 
-			target = friends.transform.GetChild(1).position;
+			target = friends.transform.GetChild(targetUnit).position;
+
 
 			enemy.GetComponent<Unit>().ChooseNewTarget(target);
 			Debug.Log(enemy.name);
