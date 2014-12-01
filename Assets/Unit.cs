0f57f3c298109df571f27252b2b800b8669023bc
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-30 20:57:35 -0500

    compressing code, deleting comments

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -10,28 +10,17 @@ public class Unit : Damagable {
     public State state;
 
     public bool Selected = false;
-    //public bool carrying = false;
-
-    //private bool moving = false;
     public Animator anim;
-
-    //const float timeLimit = 1.0f; // 1 seconds
-    //float timeAmount = timeLimit;
-    //bool timerActive = false;
-
     private int timerAttack;
     public static int attackDelay = 30;
 
     float dinoMidpointHeight;
-
 
     //public Vector3? target = null;  //nullable!
     public Vector3 target;
     private const float moveSpeed = 20f;
     private bool tooClose;
     private const float rotateSpeed = 5f;
-
-
 
 	// Use this for initialization
     public override void Start()
@@ -46,36 +35,16 @@ public class Unit : Damagable {
 
         //initialize timer
         timerAttack = attackDelay;
-
-
-
-		//
-		
         health = 100;
 	}
 
 
     public override void Update()
     {
-
         base.Update();
-        
-        //float ty = Terrain.activeTerrain.SampleHeight(new Vector3(transform.position.x, 0, transform.position.z));
-        //transform.position = new Vector3(transform.position.x, ty + dinoMidpointHeight, transform.position.z);
-        
-
         RightClick ();
-
-       if (state == State.Moving) 
-       {
-			Movement();
-	   }
-
-		if (timerAttack > 0) {
-				timerAttack--;
-		}
-
-
+        if (state == State.Moving)  {    Movement();     }
+		if (timerAttack > 0) {  timerAttack--;  }
     }
 
 	void RightClick ()
@@ -83,12 +52,9 @@ public class Unit : Damagable {
 		//right click
 		if (Input.GetMouseButtonDown (1) && Selected) {
 			Vector3 tempTarget = Selection.GetWorldPositionAtHeight (Input.mousePosition, 0f);
-
 			ChooseNewTarget (tempTarget);
 		}
 	}
-
-
 
 	public void ChooseNewTarget (Vector3 tempTarget)
 	{
@@ -100,21 +66,8 @@ public class Unit : Damagable {
 	{	
         CharacterController controller = GetComponent<CharacterController>();
 
-
-		//animation moving stuff
-
-		//anim.SetBool("moving", true);
-		//if arrives at target then { movingTowardsTarget = false; target = null; }
-		/*float distAway = Vector3.Distance (transform.position, (Vector3)target);
-		if (distAway < 1 && moveSpeed > minSpeed) {
-			tooClose = true;
-			moveSpeed /= 2;
-		}*/
-
         controller.SimpleMove(moveSpeed * (target - transform.position).normalized);
-        //Debug.Log(team.ToString() + "moving");
 	    //rotation stuff
-	    //http://docs.unity3d.com/ScriptReference/Vector3.RotateTowards.html
 	    Vector3 targetDir = (Vector3)target - transform.position;
 	    targetDir = new Vector3(targetDir.x,  0, targetDir.z);
 	    float turnStep = rotateSpeed * Time.deltaTime;
@@ -123,7 +76,6 @@ public class Unit : Damagable {
 	    transform.rotation = Quaternion.LookRotation(newDir);
 
 	}
-
 
 
     void OnTriggerStay(Collider other)
